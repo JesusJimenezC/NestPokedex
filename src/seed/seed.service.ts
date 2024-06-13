c6a7de808534d2ae1aa5fd75c5690ded7ff9c797
Jesus Jimenez Cordero <jesus.jimenezc.mx@gmail.com>
2024-06-13 05:57:30 -0600

    fix: error with eslint and build railway

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
